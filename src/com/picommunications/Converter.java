77aa6a6e7fc23a6165a7b4a7b2880b91c5e385b1
Simon Hillary <simonrhillary@gmail.com>
2016-06-20 15:35:36 +0100

    Add get/set methods to converter classes

--- src/com/picommunications/Converter.java
+++ src/com/picommunications/Converter.java
@@ -1,6 +1,9 @@
 package com.picommunications;
 
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Converter {
 
 /**
@@ -13,19 +16,69 @@ public class Converter {
 
 
 //=============================================
-//  Class Variables
-//=============================================
-
-//=============================================
 //  Instance Variables
 //=============================================
+
+    static int converterID;
+    File inputFile;
+    String inputFilePath;
+    String outputFilePath;
+    ArrayList<Boolean> options;
 
 //=============================================
 //  Constructors
 //=============================================
 
+public Converter(int id, File f, String ifp, String ofp, ArrayList<Boolean> opts){
+    this.converterID = id;
+    this.inputFile = f;
+    this.inputFilePath = ifp;
+    this.outputFilePath = ofp;
+    this.options = (ArrayList<Boolean>)opts.clone();
+    }
+
+
 //=============================================
 //  Methods
 //=============================================
 
+    public int getConverterID(){
+        return converterID;
+    }
+
+    public void setInputFilepath(String ifp){
+        this.inputFilePath = ifp;
+    }
+
+    public String getInputFilePath(){
+        return inputFilePath;
+    }
+
+    public void setInputFile(File f){
+        this.inputFile = f;
+    }
+
+    public File getInputFile(){
+        return this.inputFile;
+    }
+
+    public void setOutputDirectory(String ofp) {
+        this.outputFilePath = ofp;
+    }
+
+    public String getOutputDirectory(){
+        return outputFilePath;
+    }
+
+    public void setOptions(ArrayList<Boolean>  o){
+        this.options = o;
+    }
+
+    public ArrayList<Boolean> getOptions(){
+        return options;
+    }
+
+    public void convert(){
+
+    }
 }
